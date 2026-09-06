9b5e7d8afc97e9e5884b0d9022b466441f4d6f30
Aubrey Tavu <qadctavu@tip.edu.ph>
2026-09-06 11:18:12 +0800

    Add default temperature values to WeatherData
    Added default values for MaxTempCelsius and MinTempCelsius properties.

--- RescuAR.App/Models/WeatherData.cs
+++ RescuAR.App/Models/WeatherData.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +8,8 @@ namespace RescuAR.App.Models
     {
         public string LocationName { get; set; } = string.Empty;
         public double TemperatureCelsius { get; set; }
+        public double MaxTempCelsius { get; set; } = 31.0;
+        public double MinTempCelsius { get; set; } = 24.0;
         public int WeatherCode { get; set; }
         public string ConditionDescription { get; set; } = string.Empty;
         public string ConditionSummary { get; set; } = string.Empty;
